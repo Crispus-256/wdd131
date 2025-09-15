f47acb14bb528ddd88ab5702af53496258f9e07e
Crispus-256 <nuweamanyacrispusinfinityeneri@gmail.com>
2025-09-15 11:23:12 -0600

    Modiified files: temples.html styles/temples.css scripts/temples.js

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,8 +1,6 @@
-// Update footer with current year and last modified date
 document.getElementById('current-year').textContent = new Date().getFullYear();
 document.getElementById('last-modified').textContent = document.lastModified;
 
-// Hamburger Menu Toggle
 const hamburgerBtn = document.getElementById('hamburger-btn');
 const navMenu = document.getElementById('nav-menu');
 
